3b0d3937b99ba77cfcd7f3a08e36d982e9ba0083
AminaNisic <amina.nisic@stu.ibu.edu.ba>
2022-09-14 20:19:18 +0200

    setting up boundaries

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 //1:52:29
-console.log(collisions)
+
 const canvas = document.querySelector('canvas')
 const context=canvas.getContext('2d')
 
 canvas.width= 1005;//1005
 canvas.height = 470;//470
 
-context.fillStyle='#f8a6ff';
-context.fillRect(0,0, canvas.width, canvas.height);
+const collisionsObject=[]
+
+for (let i =0; i<collisions.length; i+=140){
+  collisionsObject.push(collisions.slice(i,i+140))
+}
+
+class Border{
+  static width=60
+  static height=60
+  constructor({position}){
+    this.position=position
+    this.width=60
+    this.height=60
+  }
+  draw(){
+    context.fillStyle='red'
+    context.fillRect(this.position.x, this.position.y, this.width, this.height)
+  }
+}
+
+const borders = []
+
+collisionsObject.forEach((row, i)=>{
+  row.forEach((symbol, j)=>{
+    if (symbol===1784){
+      borders.push(new Border({position: {
+        x: j * Border.width,
+        y: i * Border.height
+          }
+        })
+      }
+    )
+  })
+})
+console.log(borders)
 
 const image= new Image();
 image.src='./images/MadasMap.png'
@@ -19,8 +52,8 @@ playerImage.src='./images/PlayerDown.png'
   //while img isnt loaded
   //the arguments: the image ref,4 coordinates for cropping character sprite,then two for width
   //and height the usual
-
 //}
+
 //animate()
 /*image.onload=()=>{
 context.drawImage(image,-2580,-1100)
@@ -31,11 +64,9 @@ class Sprite {
     this.position = position
     this.image = image
   }
-
   draw() {
     context.drawImage(this.image,this.position.x,this.position.y)
   }
-
 }
 
 const background = new Sprite({
@@ -63,7 +94,6 @@ const keys={
 
 function animate() {
   window.requestAnimationFrame(animate)
-
   //image.onload=()=>{
   background.draw()
   //}
@@ -80,14 +110,16 @@ function animate() {
     playerImage.height
   )
 //}
-if(keys.w.pressed && lastKey==='w') background.position.y+=3
-else if(keys.s.pressed && lastKey==='s') background.position.y-=3
-else if(keys.a.pressed && lastKey==='a') background.position.x+=3
-else if(keys.d.pressed && lastKey==='d') background.position.x-=3
-}
-animate()
 
+  if(keys.w.pressed && lastKey==='w') background.position.y+=3
+  else if(keys.s.pressed && lastKey==='s') background.position.y-=3
+  else if(keys.a.pressed && lastKey==='a') background.position.x+=3
+  else if(keys.d.pressed && lastKey==='d') background.position.x-=3
+}
+
+animate()
 let lastKey=''
+
 window.addEventListener('keydown',(e)=>{
   switch(e.key) {
     case 'w':
